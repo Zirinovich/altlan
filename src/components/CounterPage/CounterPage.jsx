740c4beee26033ad79d2a4d712bfddca68eb172a
Zirinovich <zhorzhmail@gmail.com>
2017-01-16 23:45:24 +0300

    redux

--- src/components/CounterPage/CounterPage.jsx
+++ src/components/CounterPage/CounterPage.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
 import StateCounter from './StateCounter';
 
+import ReduxCounter from './ReduxCounter';
+
 class CounterPage extends Component {
     render() {
         return (
@@ -9,6 +11,8 @@ class CounterPage extends Component {
                 <PageHeader>Counters</PageHeader>
                 <h3>State Counter</h3>
                 <StateCounter />
+                <h3>Redux Counter</h3>
+                <ReduxCounter />
             </div>
         );
     }
--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ import ReactDom from 'react-dom/server';
 import {match, RouterContext} from 'react-router';
 import routes from './routes';
 
+import {Provider} from 'react-redux';
+import configureStore from './redux/configureStore';
+
 const app = express();
 
 app.use((req, res) => {
+    const store = configureStore();
 
     match({routes, location: req.url}, (error, redirectLocation, renderProps) => { // функция match принимает в качестве первого параметра JavaScript объект с ключами routes и location { routes: routes, location: req.url}
         if (redirectLocation) { // Если необходимо сделать redirect
@@ -21,7 +25,11 @@ app.use((req, res) => {
             return res.status(404).send('Not found');
         }
 
-        const componentHTML = ReactDom.renderToString(<RouterContext {...renderProps} />);
+        const componentHTML = ReactDom.renderToString(
+            <Provider store={store}>
+                <RouterContext {...renderProps} />
+            </Provider>
+        );
 
         return res.end(renderHTML(componentHTML));
     });
