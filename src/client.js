4187a7674d5012e2c7e99ab850e1436b3087226d
Zirinovich <zhorzhmail@gmail.com>
2017-01-22 17:52:28 +0300

    Redux devtools

--- src/client.js
+++ src/client.js
@@ -6,6 +6,8 @@ import routes from './routes';
 import {Provider} from 'react-redux';
 import configureStore from './redux/configureStore';
 
+import DevTools from './components/DevTools'; // redux инструменты разработчика
+
 const store = configureStore();
 
 const component = (
@@ -17,3 +19,4 @@ const component = (
 );
 
 ReactDOM.render(component, document.getElementById('react-view'));
+ReactDOM.render(<DevTools store={store} />, document.getElementById('dev-tools'));
